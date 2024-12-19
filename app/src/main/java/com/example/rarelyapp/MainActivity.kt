ca178163c1aad9dbf9184b79ff168e3389a6145c
Perimsu <perimsutukuc@gmail.com>
2024-12-19 11:10:01 +0300

    Created VerificationActivity.kt modifications will be made after authentication part is complete

--- app/src/main/java/com/example/rarelyapp/MainActivity.kt
+++ app/src/main/java/com/example/rarelyapp/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.rarelyapp.ui.authentication.VerificationScreen
 import com.example.rarelyapp.ui.theme.RarelyAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,28 +21,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             RarelyAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    VerificationScreen()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    RarelyAppTheme {
-        Greeting("Android")
-    }
-}
--- app/src/main/java/com/example/rarelyapp/ui/theme/Color.kt
+++ app/src/main/java/com/example/rarelyapp/ui/theme/Color.kt
@@ -2,6 +2,11 @@ package com.example.rarelyapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+val DarkBlue = Color(0xFF18233D)
+val BackgroundColor = Color(0xFF18233D)
+val TextColor = Color(0xFF4E4E50)
+
+
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
